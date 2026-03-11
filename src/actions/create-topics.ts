14bbafd1bac48c92312e4bc934417d6fe5ffa9fc
manoj30dec <manoj.kumar@nagarro.com>
2026-03-11 15:04:27 +0530

    page creations

---
+++ src/actions/create-topics.ts
@@ -0,0 +1,5 @@
+"use server"
+
+export async function createTopics() {
+    // todo: revalidate the home page after create topic
+}
---
+++ src/app/topics/[slug]/page.tsx
@@ -0,0 +1,4 @@
+
+export default function TopicShowPage() {
+    return <div>topic show</div>
+}
