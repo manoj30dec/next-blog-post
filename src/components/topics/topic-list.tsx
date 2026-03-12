17d0b27c97f6e2a4a22a576b5d5e67012bd5c388
manoj30dec <manoj.kumar@nagarro.com>
2026-03-12 14:21:05 +0530

    post create feature

---
+++ src/components/topics/topic-list.tsx
@@ -0,0 +1,21 @@
+import { db } from "@/db";
+import Link from "next/link";
+import { Chip } from "@nextui-org/react";
+import paths from "@/path";
+export default async function TopicList() {
+    const topics = await db.topic.findMany();
+    const renderedTopic = topics.map((topic) => {
+        return (
+            <div key={topic.id}>
+                <Link href={paths.topicShow(topic.slug)}>
+                    <Chip color="warning" variant="shadow">
+                        {topic.slug}
+                    </Chip>
+                </Link>
+            </div>
+        )
+    })
+    return <div className="flex flex-row flex-wrap gap-2">
+        {renderedTopic}
+    </div>
+}
